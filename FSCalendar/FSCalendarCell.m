cdd482843cc4601b28ba960672810f41621e5518
“kevin.chen” <kevin.chen@medicrowd.com.tw>
2024-01-24 17:24:14 +0800

    change cell height

--- FSCalendar/FSCalendarCell.m
+++ FSCalendar/FSCalendarCell.m
@@ -132,11 +132,12 @@
     
     CGFloat titleHeight = self.bounds.size.height*5.0/6.0;
     CGFloat diameter = MIN(self.bounds.size.height*5.0/6.0,self.bounds.size.width);
-    diameter = diameter > FSCalendarStandardCellDiameter ? (diameter - (diameter-FSCalendarStandardCellDiameter)*0.5) : diameter;
-    _shapeLayer.frame = CGRectMake((self.bounds.size.width-diameter)/2,
-                                   (titleHeight-diameter)/2,
-                                   diameter,
-                                   diameter);
+    //    diameter = diameter > FSCalendarStandardCellDiameter ? (diameter - (diameter-FSCalendarStandardCellDiameter)*0.5) : diameter;
+        diameter = 48;
+        _shapeLayer.frame = CGRectMake((self.bounds.size.width-diameter)/2,
+                                       (titleHeight-diameter)/2 - 5,
+                                       diameter,
+                                       75);
     
     CGPathRef path = [UIBezierPath bezierPathWithRoundedRect:_shapeLayer.bounds
                                                 cornerRadius:CGRectGetWidth(_shapeLayer.bounds)*0.5*self.borderRadius].CGPath;
